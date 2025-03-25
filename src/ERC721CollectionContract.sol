a175a3ffe20efa1a41d1d8660bbabf25068c45d4
rathodvinod5 <rathodvinod5@gmail.com>
2025-03-25 22:30:56 +0530

    added collection factory and collection creation contract

--- src/ERC721CollectionContract.sol
+++ src/ERC721CollectionContract.sol
@@ -5,7 +5,16 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ERC721CollectionContract is ERC721URIStorage, Ownable {
-    constructor() ERC721() {
+    uint256 public tokenIdCounter;
 
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) Ownable(msg.sender) {
+        transferOwnership(msg.sender);
+    }
+
+    function createCollection(string memory tokenURI) public {
+        tokenIdCounter++;
+        uint256 newTokenId = tokenIdCounter;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
     }
 }
--- src/NFTMarketplace.sol
+++ src/NFTMarketplace.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.13;
 
+import "./ERC721CollectionContract.sol";
+
 contract NFTMarkeplace {
     struct Collection {
         string name;
@@ -13,8 +15,20 @@ contract NFTMarkeplace {
     Collection[] public collections;
     mapping(address => Collection[]) public userCollections;
 
+    event CollectionCreated(string name, string description, address contractAddress, address creator);
+
     function createNewCollection(string memory name, string memory symbol) public {
-        
+        ERC721CollectionContract contractAddress = new ERC721CollectionContract(name, symbol);
+        Collection memory newCollection = Collection({
+            name: name,
+            description: "",
+            image: "",
+            contractAddress: address(contractAddress),
+            creator: msg.sender
+        });
+        collections.push(newCollection);
+        userCollections[msg.sender].push(newCollection);
+        emit CollectionCreated(name, "", address(contractAddress), msg.sender);
     }
 
     function getAllCollections() public view returns(Collection[] memory) {
