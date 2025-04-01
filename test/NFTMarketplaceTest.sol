acfcc0ea1160c470e9f1befd04e1c5319a6bf511
rathodvinod5 <rathodvinod5@gmail.com>
2025-04-01 14:29:29 +0530

    updated marketplace contract to handle edge cases

---
+++ test/NFTMarketplaceTest.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/Marketplace.sol";
+
+contract NFTMarketPlaceTest is Test {
+    NFTMarketplace nftMarketplace;
+
+    function setUp() public {
+
+    }
+}
